13fc8627ab8ddc0cc4b08d33ff9f4a2d8fe3ccc0
ddebroy08 <3015468780101@ingenieria.usac.edu.gt>
2022-09-01 12:47:12 -0600

    Primer commit

---
+++ src/entrar/Entrar.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package entrar;
+
+/**
+ *
+ * @author Diego Debroy
+ */
+public class Entrar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ingresar ingresar = new Ingresar();
+        ingresar.setVisible(true);
+        
+    }
+    
+}
